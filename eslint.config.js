1a30864a74282aa61ff0a1371f4f7147ae6b5d72
granawkins <granthawkins88@gmail.com>
2024-11-12 11:25:20 -0800

    add vitest

--- eslint.config.js
+++ eslint.config.js
@@ -5,23 +5,32 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config({ ignores: ['dist'] }, prettier, {
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default tseslint.config(
+  { ignores: ['dist'] },
+  prettier,
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'max-len': 'off',
+      'no-mixed-spaces-and-tabs': 'off',
+      'no-trailing-spaces': 'off',
+    },
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    // Turn off rules that might conflict with Prettier
-    'max-len': 'off',
-    'no-mixed-spaces-and-tabs': 'off',
-    'no-trailing-spaces': 'off',
-  },
-});
+  {
+    files: ['**/*.test.{ts,tsx}', '**/test/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  }
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Contact from './components/Contact';
-import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
@@ -13,7 +12,6 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
